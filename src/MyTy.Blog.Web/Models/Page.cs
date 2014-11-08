c46dc2b3b75b4bee65ef919fcb335f8ddf3820d4
Michael Tyson <michaeltyson@outlook.com>
2014-11-08 12:23:56 -0500

    add missing page model

---
+++ src/MyTy.Blog.Web/Models/Page.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTy.Blog.Web.Models
+{
+	public class Page
+	{
+		public string FileLocation { get; set; }
+		public string Href { get; set; }
+		public string Title { get; set; }
+		public string SubTitle { get; set; }
+		public string HeaderBackgroundImage { get; set; }
+		public DateTime Date { get; set; }
+		public string Content { get; set; }
+		public string Layout { get; set; }
+	}
+}
